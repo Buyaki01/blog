94a37dc9f9429f1c7e6a25b21a0d363ba61b9858
Buyaki <rittahbuyaki@gmail.com>
2021-04-19 00:21:24 +0300

    Add edit,update,destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,25 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
 
   private
     def article_params
